39b4fa9dda90f7ec6ae2d2749c8c702bf1b23752
אלחנן טוויג <tweig@g.jct.ac.il>
2021-10-06 22:49:17 +0300

    intial code + welcome functions

---
+++ Targil0/Program8680.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            welcome8680();
+            welcome5588();
+        }
+
+        private static void welcome8680()
+        {
+            Console.WriteLine("Enter your name: ");
+            string s = Console.ReadLine();
+            Console.WriteLine($"{s}, welcome to my first console application");
+        }
+
+        private static void welcome5588()
+        {
+
+        }
+    }
+}
