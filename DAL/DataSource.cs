9545bb190e904a9471df3fd120694d7d8eab9f2a
אלחנן טוויג <tweig@g.jct.ac.il>
2021-10-24 21:32:00 +0300

    continued the "InitializeParsel"

--- DAL/DataSource.cs
+++ DAL/DataSource.cs
@@ -14,18 +14,19 @@ namespace DalObject
         internal static List<Customer> customers = new List<Customer>(100);
         internal static List<Parcel> parcels = new List<Parcel>(1000);
 
-        internal static Random rand = new Random();
-        internal static int randCustomer = rand.Next(210000000, 340000000);
-        internal static int randDrone = rand.Next(1000, 10001);
-        internal static int 
+        
 
-        //internal class Config
-        //{
-        //    internal static int IndexDrone = 0;
-        //    internal static int IndexStation = 0;
-        //    internal static int IndexCustomer = 0;
-        //    internal static int IndexParcel = 0;
-        //}
+        internal class Config
+        {
+            //internal static int IndexDrone = 0;
+            //internal static int IndexStation = 0;
+            //internal static int IndexCustomer = 0;
+            //internal static int IndexParcel = 0;
+            internal static Random rand = new Random();
+            internal static int randCustomer = rand.Next(210000000, 340000000);
+            internal static int randDrone = rand.Next(1000, 10001);
+            internal static int SerialNum = 0;
+        }
         internal static void Initialize()
         {
             InitializeDrone();
@@ -39,71 +40,72 @@ namespace DalObject
             {
                 drones.Add(new Drone
                 {
-                    Battery = rand.Next(101),
-                    Id = randDrone,
-                    Model = (ModelDrones)rand.Next(),
-                    Status = (DroneStatuses)rand.Next(),
-                    MaxWeight = (WeightCategory)rand.Next()
+                    Battery = Config.rand.Next(101),
+                    Id = Config.randDrone,
+                    Model = (ModelDrones)Config.rand.Next(),
+                    Status = (DroneStatuses)Config.rand.Next(),
+                    MaxWeight = (WeightCategory)Config.rand.Next()
                 });
             }
         }
         public static void InitializeStation()
         {
-            stations = new List<Station>()
+            stations.Add(new Station
             {
-                new Station
-                {
-                    Id = 1010,
-                    Name = "Malcha Mall",
-                    Lattitude = 31.751716,
-                    Longitude = 35.187202,
-                    ChargeSolts = rand.Next(10)
-                },
-                new Station
-                {
-                        Id = 1020,
-                        Name = "Hadar Mall",
-                        Lattitude = 31.753791,
-                        Longitude = 35.213429,
-                        ChargeSolts = rand.Next(10)
-                },
-                new Station
-                    {
-                        Id = 1030,
-                        Name = "Ramot Mall",
-                        Lattitude = 31.817627,
-                        Longitude = 35.194476,
-                        ChargeSolts = rand.Next(10)
-                    },
-                new Station
-                    {
-                        Id = 1040,
-                        Name = "Jerusalem Central Station",
-                        Lattitude = 31.789061,
-                        Longitude = 35.203100,
-                        ChargeSolts = rand.Next(10)
-                    },
-                new Station
-                    {
-                        Id = 1050,
-                        Name = "Mamila Mall",
-                        Lattitude = 31.777870,
-                        Longitude = 35.224982,
-                        ChargeSolts = rand.Next(10)
-                    }
-            };
+                Id = 1010,
+                Name = "Malcha Mall",
+                Lattitude = 31.751716,
+                Longitude = 35.187202,
+                ChargeSolts = Config.rand.Next(10)
+            });
+
+            stations.Add(new Station
+            {
+                Id = 1020,
+                Name = "Hadar Mall",
+                Lattitude = 31.753791,
+                Longitude = 35.213429,
+                ChargeSolts = Config.rand.Next(10)
+            });
+
+            stations.Add(new Station
+            {
+                Id = 1030,
+                Name = "Ramot Mall",
+                Lattitude = 31.817627,
+                Longitude = 35.194476,
+                ChargeSolts = Config.rand.Next(10)
+            });
+
+            stations.Add(new Station
+            {
+                Id = 1040,
+                Name = "Jerusalem Central Station",
+                Lattitude = 31.789061,
+                Longitude = 35.203100,
+                ChargeSolts = Config.rand.Next(10)
+            });
+
+            stations.Add(new Station
+            {
+                Id = 1050,
+                Name = "Mamila Mall",
+                Lattitude = 31.777870,
+                Longitude = 35.224982,
+                ChargeSolts = Config.rand.Next(10)
+            });
         }
         public static void InitializeCustomer()
         {
             for (int i = 0; i < 10; i++)
             {
-                customers.Add(new Customer
+                customers.Add( new Customer
                 {
-                    Id = randCustomer,
-                    Phone = rand.Next(0500000000, 0590000000),
-                    Name = (Names)rand.Next(),
-                    Longitude = (double)rand.Next(31737458, 35174572) / (double)100000,
-                    Latittude = (double)rand.Next(31807238, 35241141) / (double)100000
+                    Id = Config.randCustomer,
+                    Phone = Config.rand.Next(0500000000, 0590000000),
+                    Name = (Names)Config.rand.Next(),
+                    Longitude = (double)Config.rand.Next(31737458, 35174572) / (double)100000,
+                    Latittude = (double)Config.rand.Next(31807238, 35241141) / (double)100000
                 });
             }
         }
@@ -114,18 +116,19 @@ namespace DalObject
             {
                 new Parcel
                 {
-                    Id = 
-                    SenderId = rand.Next(210000000, 340000000),
-                    TargetId = randCustomer,
-                    DroneId = randDrone,
+                    Id = Config.SerialNum,
+                    SenderId = Config.rand.Next(210000000, 340000000),
+                    TargetId = Config.randCustomer,
+                    DroneId = Config.randDrone,
                     Requested = newDate,
-                    Scheduled = newDate.AddMinutes(rand.Next(15,30)),
-                    PickedUp = newDate.AddMinutes(rand.Next(12,40)),
-                    Delivered = newDate.AddMinutes(rand.Next(20,45)),
-                    Weight = (WeightCategory)rand.Next(3),
-                    Priorities = (Priority)rand.Next(3)
+                    Scheduled = newDate.AddMinutes(Config.rand.Next(15,30)),
+                    PickedUp = newDate.AddMinutes(Config.rand.Next(12,40)),
+                    Delivered = newDate.AddMinutes(Config.rand.Next(20,45)),
+                    Weight = (WeightCategory)Config.rand.Next(3),
+                    Priorities = (Priority)Config.rand.Next(3)
                 }
             };
+            Config.SerialNum++;
         }
     }
 }
