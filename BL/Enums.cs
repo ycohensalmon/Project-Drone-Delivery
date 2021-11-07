8559cb85603b1f59e01b3e334bdbaee12948e4a0
Yossef Cohen-Salmon <cohensal@g.jct.ac.il>
2021-11-08 00:59:41 +0200

    add enums

--- BL/Enums.cs
+++ BL/Enums.cs
@@ -5,7 +5,7 @@ namespace IBL
     namespace BO
     {
         public enum WeightCategory { Light, Medium, Heavy }
-        //public enum DroneStatuses { Available, Maintenance, Delivery }
+        public enum DroneStatuses { Available, Maintenance, Delivery }
         public enum Priority { Normal, Fast, Emergency }
         public enum ModelDrones { Cobra, Magic, Mavic_Air, DJI, Mickcara }
         public enum Names
