72b3f74c645f453488ba7d59bf9e1e9754f01828
Yossef Cohen-Salmon <cohensal@g.jct.ac.il>
2021-11-22 01:31:27 +0200

    BL partial

--- BL/BL.cs
+++ BL/BL.cs
@@ -11,78 +11,31 @@ namespace IBL
 {
     namespace BO
     {
-        public class BL : IBL
+        public partial class BL : IBL
         {
-            Random rand = new Random();
-            public IDal dalObj = new DalObject.DalObject();
-            public ParcelInList ParcelList;// = IDAL.DO.Parcel;
-            List<Drone> drones = new();
-            public BL()
+            public void NewStation(Station x)
             {
-                IEnumerable<IDAL.DO.Drone> drone = dalObj.GetDrones();
-                IEnumerable<IDAL.DO.Station> station = dalObj.GetStations();
-                IEnumerable<IDAL.DO.Customer> customer = dalObj.GetCustomers();
-                IEnumerable<IDAL.DO.Parcel> parcel = dalObj.GetParcels();
-                IEnumerable<IDAL.DO.DroneCharge> droneCharges = dalObj.GetDroneCharges();
-
-                foreach (var tempDrone in drone)
-                {
-                    DroneStatuses statuses = GetStatus(tempDrone.Id, parcel);
-
-                    drones.Add(new Drone
-                    {
-                        Id = tempDrone.Id,
-                        Model = tempDrone.Model,
-                        MaxWeight = (WeightCategory)tempDrone.MaxWeight,
-                        Status = statuses,
-                        Battery = GetBattery(statuses),
-                        //ParcelInTravel =
-                        Location = GetLocation(statuses, tempDrone.Id, parcel)
-                    });
-                }
+                throw new NotImplementedException();
             }
 
-            private Location GetLocation(DroneStatuses statuses, int droneId, IEnumerable<IDAL.DO.Parcel> parcel)
+            public void NewCostumer(Customer x)
             {
-                IDAL.DO.Parcel tempParcel = GetTempParcel(droneId, parcel);
-                if (tempParcel.DroneId == droneId)
-                {
-                    // החבילה שויכה ולא נאספה
-                    if (tempParcel.Scheduled != DateTime.MinValue && tempParcel.PickedUp == DateTime.MinValue)
-                    {
-                        // מיקום - תחנה הקרובה לשולח
-                    }
-                    // חבילה נאספה אך לא סופקה
-                    else if (tempParcel.PickedUp != DateTime.MinValue && tempParcel.Delivered == DateTime.MinValue)
-                    {
-                        // מיקום - מיקום השולח
-                    }
-                }
-
-                if (statuses == DroneStatuses.Available)
-                {
-                    List<IDAL.DO.Parcel> parcelDelivered = new();
-                    foreach (var item in parcel) if(item.Delivered != DateTime.MinValue){parcelDelivered.Add(item);}
-                }
+                throw new NotImplementedException();
             }
 
-            private double GetBattery(DroneStatuses status)
+            public void NewDrone(Drone x, int numStation)
             {
-                if (status != DroneStatuses.Maintenance) return rand.Next(30, 100);
-                else return rand.Next(0, 20);
+                throw new NotImplementedException();
             }
 
-            private DroneStatuses GetStatus(int droneId, IEnumerable<IDAL.DO.Parcel> parcel)
+            public void NewParcel(Parcel x, int senderID, int receiveID)
             {
-                if (GetTempParcel(droneId, parcel).DroneId == droneId) 
-                    return DroneStatuses.Delivery;
-                else
-                    return (DroneStatuses)rand.Next(2);
+                throw new NotImplementedException();
             }
 
-            private IDAL.DO.Parcel GetTempParcel(int droneId, IEnumerable<IDAL.DO.Parcel> parcel)
+            public void sendDroneToCharge(int droneID, int baseStatiunID)
             {
-                return parcel.First(parcel => parcel.DroneId == droneId);
+                throw new NotImplementedException();
             }
         }
     }
