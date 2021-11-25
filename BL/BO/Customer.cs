fb53b097ea6842598bbac3684ac7b275d1c047b4
Yossef Cohen-Salmon <cohensal@g.jct.ac.il>
2021-11-25 17:39:47 +0200

    BO

---
+++ BL/BO/Customer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBL
+{
+    namespace BO
+    {
+        public class Customer
+        {
+            private Int32 id;
+            private string name;
+            private Int32 phone;
+            private Location location;
+            private List<ParcelAtCustomer> parcelFromCustomer = new();
+            private List<ParcelAtCustomer> parcelsToCustomer = new();
+
+            public Int32 Id { get => id; set => id = value; }
+            public string Name { get => name; set => name = value; }
+            public Int32 Phone { get => phone; set => phone = value; }
+            public Location Location { get => location; set => location = value; }
+            public List<ParcelAtCustomer> ParcelsFromCustomer { get => parcelFromCustomer; set => parcelFromCustomer = value; }
+            public List<ParcelAtCustomer> ParcelsToCustomer { get => parcelsToCustomer; set => parcelsToCustomer = value; }
+            public override string ToString()
+            {
+                return $"Id: {Id}, Name:{Name}, Phone:{Phone}, Location:{Location}, ParcelsFromCustomer:{ParcelsFromCustomer}, ParcelsToCustomer:{ParcelsToCustomer}";
+            }
+        }
+    }
+}
---
+++ BL/BO/Station.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBL
+{
+    namespace BO
+    {
+        public class Station
+        {
+            private int id;
+            private string name;
+            private Location location;
+            private int chargeSolts;
+            private List<DroneCharge> droneCharges = new();
+
+            public int Id { get => id; set => id = value; }
+            public string Name { get => name; set => name = value; }
+            public Location Location { get => location; set => location = value; }
+            public int ChargeSolts { get => chargeSolts; set => chargeSolts = value; }
+            public List<DroneCharge> DroneCharges { get => droneCharges; set => droneCharges = value; }
+            public override string ToString()
+            {
+                return string.Format($"Id: {Id}\nName: {Name}\nLocation: {Location}\nAvailable charging positions: {ChargeSolts}\nDroneCharges: {DroneCharges}");
+            }
+        }
+    }
+}
