14dd7d32d766b7de94701cf62eadf9d37da42dc6
Yossef Cohen-Salmon <cohensal@g.jct.ac.il>
2021-11-27 22:13:17 +0200

    some changes

--- BL/BO/Station.cs
+++ BL/BO/Station.cs
@@ -17,6 +17,9 @@ namespace IBL
             public List<DroneCharge> DroneCharges { get; set; } = new();
             public override string ToString()
             {
+                if(DroneCharges == null)
+                    return string.Format($"Id: {Id}\nName: {Name}\nLocation: {Location}\nAvailable charging positions: {ChargeSolts}\nDroneCharges: 0");
+
                 return string.Format($"Id: {Id}\nName: {Name}\nLocation: {Location}\nAvailable charging positions: {ChargeSolts}\nDroneCharges: {DroneCharges}");
             }
         }
