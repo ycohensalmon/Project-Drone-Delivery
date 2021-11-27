ed7f9739918d2064789feb1a579d537be6a3f1d6
אלחנן טוויג <tweig@g.jct.ac.il>
2021-11-28 01:18:10 +0200

    parcel

--- BL/BO/Parcel.cs
+++ BL/BO/Parcel.cs
@@ -10,31 +10,25 @@ namespace IBL
     {
         public class Parcel
         {
-            private int id;
-            private CustomerInParcel sender;
-            private CustomerInParcel target;
-            private DroneInParcel drone;
-            private DateTime requested;
-            private DateTime scheduled;
-            private DateTime pickedUp;
-            private DateTime delivered;
-            private WeightCategory weight;
-            private Priority priorities;
-
-            public int Id { get => id; set => id = value; }
-            public CustomerInParcel Sender { get => sender; set => sender = value; }
-            public CustomerInParcel Target { get => target; set => target = value; }
-            public DroneInParcel Drone { get => drone; set => drone = value; }
-            public DateTime Requested { get => requested; set => requested = value; }
-            public DateTime Scheduled { get => scheduled; set => scheduled = value; }
-            public DateTime PickedUp { get => pickedUp; set => pickedUp = value; }
-            public DateTime Delivered { get => delivered; set => delivered = value; }
-            public WeightCategory Weight { get => weight; set => weight = value; }
-            public Priority Priorities { get => priorities; set => priorities = value; }
+            public int Id { get; set; }
+            public CustomerInParcel Sender { get; set; }
+            public CustomerInParcel Target { get; set; }
+            public DroneInParcel Drone { get; set; }
+            public DateTime Requested { get; set; }
+            public DateTime Scheduled { get; set; }
+            public DateTime PickedUp { get; set; }
+            public DateTime Delivered { get; set; }
+            public WeightCategory Weight { get; set; }
+            public Priority Priorities { get; set; }
             public override string ToString()
             {
-                return $"Id: {Id}, Sender: {Sender}, Target: {Target}, DroneId: {Drone}\nRequested: {Requested}, " +
-                    $"Scheduled: {Scheduled}\nPickedUp:  {PickedUp}, Delivered: {Delivered}\nWeight: {Weight}, Priorities:{Priorities}\n";
+                if (Drone.Id != 0)
+                {
+                    return $"Id: {Id}, \nSender: {Sender} \nTarget: {Target} \nDroneId: {Drone}\nRequested: {Requested} " +
+                        $"\nScheduled: {Scheduled}\nPickedUp:  {PickedUp} \nDelivered: {Delivered}\nWeight: {Weight} Priorities:{Priorities}\n";
+                }
+                return $"Id: {Id} \nSender: {Sender} \nTarget: {Target} \nRequested: {Requested} " +
+                    $"\nScheduled: {Scheduled}\nPickedUp:  {PickedUp} \nDelivered: {Delivered}\nWeight: {Weight} \nPriorities:{Priorities}\n";
             }
         }
     }
