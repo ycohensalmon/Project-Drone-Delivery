9f7bf5e79d83fef5297dac4d2e8329eea9d898a1
אלחנן טוויג <tweig@g.jct.ac.il>
2021-12-29 00:43:05 +0200

    Exceptions

--- BL/BL/BLprint.cs
+++ BL/BL/BLprint.cs
@@ -284,19 +284,20 @@ namespace BL
         {
             IEnumerable<string> names = from x in dalObj.GetCustomers()
                                         select x.Name;
-            return (IEnumerable<string>)names.Select(x=>x);
+            return names.Count() == 0 ? throw new EmptyListException("customers") : names.Select(x => x);
         }
 
         public IEnumerable<string> GetNamesOfAvailableChargeSolts()
         {
             IEnumerable<string> names = from x in GetStationWithChargeSolts()
                                         select x.Name;
-            return names.Count() == 0 ? throw new  : names;  
+            return names.Count() == 0 ? throw new EmptyListException("available chargeSolts") : names.Select(x => x);
         }
 
         public int GetStationIdByName(string name)
         {
-            return dalObj.GetStations().FirstOrDefault(x => x.Name == name).Id;
+            int id = dalObj.GetStations().FirstOrDefault(x => x.Name == name).Id;
+            return id == 0 ? throw new IncorectInputException("name of the station") : id;
         }
 
         public int GetCustomerIdByName(string name)
