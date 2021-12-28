71b5192be62f9fe02b39374a7dbe79e76feb29dc
אלחנן טוויג <tweig@g.jct.ac.il>
2021-12-28 23:58:15 +0200

    let

--- BL/BL/BLprint.cs
+++ BL/BL/BLprint.cs
@@ -190,22 +190,18 @@ namespace BL
 
         public IEnumerable<StationList> GetStations(Predicate<StationList> predicate = null)
         {
-            List<StationList> stations = new();
-
-            foreach (var item in dalObj.GetStations())
-            {
-                Station station = GetStationById(item.Id);
-                stations.Add(new StationList
-                {
-                    Id = station.Id,
-                    Name = station.Name,
-                    ChargeSoltsAvailable = station.ChargeSolts,
-                    ChargeSoltsBusy = station.DroneCharges.Count()
-                });
-            }
+            var stations = from item in dalObj.GetStations()
+                    let station = GetStationById(item.Id)
+                    select new StationList
+                    {
+                        Id = station.Id,
+                        Name = station.Name,
+                        ChargeSoltsAvailable = station.ChargeSolts,
+                        ChargeSoltsBusy = station.DroneCharges.Count()
+                    };
 
             if (predicate != null)
-                stations = stations.FindAll(x => predicate(x));
+                stations = stations.ToList().FindAll(x => predicate(x));
 
             if (!stations.Any()) 
                 throw new EmptyListException("stations");
@@ -295,7 +291,7 @@ namespace BL
         {
             IEnumerable<string> names = from x in GetStationWithChargeSolts()
                                         select x.Name;
-            return names;
+            return names.Count() == 0 ? throw new  : names;  
         }
 
         public int GetStationIdByName(string name)
