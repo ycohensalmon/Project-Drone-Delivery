78729d6109dfdcf2b44c846a7d9520f2a69b339b
Yossef Cohen-Salmon <cohensal@g.jct.ac.il>
2022-01-02 23:08:43 +0200

    xml exception

---
+++ DAL/DO/XmlException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DO
+{
+    public class XmlFileCreateException : Exception
+    {
+        public XmlFileCreateException(string xmlPath) : base($"ERROR: fail to create xml file: {xmlPath}") { }
+    }
+
+    public class XmlFileLoadException : Exception
+    {
+        public XmlFileLoadException(string xmlPath) : base($"ERROR: fail to load xml file: {xmlPath}") { }
+    }
+}
